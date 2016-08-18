da90bbd7be3492c65fc718ed91112ca2c5564654
ArturoOntiveros <arturo.o.s@outlook.com>
2016-08-18 15:38:01 -0500

    test en task y list

--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -4,4 +4,17 @@ class ListTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  def setup
+    	@list = lists(:one)
+  end
+
+  test "valida que el campo title no sea vacio" do
+  	assert_not_empty(@list.title, 'Title es vacio')
+  end
+
+  test "validando que no sea task null" do
+  	assert_not_nil(@list, 'Task es null')
+  end
+
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -4,4 +4,17 @@ class TaskTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+  	@alumn = alumnos(:three)
+  end
+
+  test "valida que el campo nombre no sea vacio" do
+  	assert_not_empty(@alumno1.nombre, 'El alumno es vacio')
+  end
+
+  test "validando que no sea alumno null" do
+  	assert_not_nil(@alumno1, 'El alumno es null')
+  end
+
 end
