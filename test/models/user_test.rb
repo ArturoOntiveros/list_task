732c324a657ddf5f069a55af74b2697c8a0099b8
ArturoOntiveros <arturo.o.s@outlook.com>
2016-08-18 15:56:36 -0500

    al borrar usuario, borrar listas

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,36 +10,42 @@ class UserTest < ActiveSupport::TestCase
  	@user2 = users(:two)
   end
 
-  test "valida que el campo name no sea vacio" do
+  test 'valida que el campo name no sea vacio' do
   	assert_not_empty(@user.name, 'name es vacio')
   end
 
-  test "valida que el campo name no sea vacio" do
+  test 'valida que el campo name no sea vacio' do
   	assert_not_empty(@user2.name, 'name es vacio')
   end
 
-  test "valida que el campo lastname no sea vacio" do
+  test 'valida que el campo lastname no sea vacio' do
   	assert_not_empty(@user.lastname, 'name es vacio')
   end
 
-  test "valida que el campo lastname no sea vacio" do
+  test 'valida que el campo lastname no sea vacio' do
   	assert_not_empty(@user2.lastname, 'name es vacio')
   end
 
-  test "valida que el campo username no sea vacio" do
+  test 'valida que el campo username no sea vacio' do
   	assert_not_empty(@user.username, 'name es vacio')
   end
 
-  test "valida que el campo username no sea vacio" do
+  test 'valida que el campo username no sea vacio' do
   	assert_not_empty(@user2.username, 'name es vacio')
   end
 
-  test "valida que el campo e-mail no sea vacio" do
+  test 'valida que el campo e-mail no sea vacio' do
   	assert_not_empty(@user.name, 'name es vacio')
   end
 
-  test "valida que el campo e-mail no sea vacio" do
+  test 'valida que el campo e-mail no sea vacio' do
   	assert_not_empty(@user2.name, 'name es vacio')
+  end
+
+
+  test 'borrar user en cascada' do
+    @user.destroy
+    assert_empty User.where(one:@user.name)
   end
 
 end
